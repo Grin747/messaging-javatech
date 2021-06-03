2e025b729b0fe152bf2071d7e6be1835aa008096
Grin <grinlikegreenbutgrin@gmail.com>
2021-06-03 07:57:21 +0500

    jms task done

--- src/main/java/com/example/messagingrabbitmq/MessagingRabbitmqApplication.java
+++ src/main/java/com/example/messagingrabbitmq/MessagingRabbitmqApplication.java
@@ -1,9 +1,6 @@
 package com.example.messagingrabbitmq;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
@@ -14,23 +11,22 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class MessagingRabbitmqApplication {
 
-	static final String topicExchangeName = "spring-boot-exchange";
-
+	static final String exchangeName = "spring-boot-exchange";
 	static final String queueName = "spring-boot";
 
 	@Bean
 	Queue queue() {
-		return new Queue(queueName, false);
+		return new Queue(queueName, false, true, true);
 	}
 
 	@Bean
-	TopicExchange exchange() {
-		return new TopicExchange(topicExchangeName);
+	FanoutExchange exchange() {
+		return new FanoutExchange(exchangeName, false, true);
 	}
 
 	@Bean
-	Binding binding(Queue queue, TopicExchange exchange) {
-		return BindingBuilder.bind(queue).to(exchange).with("foo.bar.#");
+	Binding binding(Queue queue, FanoutExchange exchange) {
+		return BindingBuilder.bind(queue).to(exchange);
 	}
 
 	@Bean
